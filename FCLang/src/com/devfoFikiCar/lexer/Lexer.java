922b655849d27e40a6cffe1016f6a47f830b0d90
devfoFikiCar <fiki.obradovic@gmail.com>
2020-09-04 09:42:10 +0200

    Line number to token

--- FCLang/src/com/devfoFikiCar/lexer/Lexer.java
+++ FCLang/src/com/devfoFikiCar/lexer/Lexer.java
@@ -6,18 +6,18 @@ import com.devfoFikiCar.main;
 import java.util.ArrayList;
 
 public class Lexer {
-    public static int posInLine = 0;
+    public static int lineNumber = -1;
 
-    public static ArrayList<Token> lexer(){
+    public static ArrayList<Token> lexer() {
         ArrayList<Token> tokens = new ArrayList<>();
-        for(int line = 0; line < main.code.size(); line++){
+        for (int line = 0; line < main.code.size(); line++) {
+            lineNumber++;
             String currentLine = main.code.get(line) + " ";
             String temp = "";
             boolean skip = false;
-            for(int i = 0; i < currentLine.length(); i++){
-                posInLine = i;
+            for (int i = 0; i < currentLine.length(); i++) {
                 skip = false;
-                switch (currentLine.charAt(i)){
+                switch (currentLine.charAt(i)) {
                     case ' ': {
                         tokens.add(lexTemp(temp));
                         temp = "";
@@ -25,15 +25,15 @@ public class Lexer {
                         break;
                     }
                     case '=': {
-                        if(currentLine.charAt(i + 1) == '='){
+                        if (currentLine.charAt(i + 1) == '=') {
                             i++;
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("EQUAL_TO", "=="));
+                            tokens.add(new Token("EQUAL_TO", "==",  lineNumber));
                             temp = "";
                             skip = true;
                         } else {
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("EQUALS", "="));
+                            tokens.add(new Token("EQUALS", "=",  lineNumber));
                             temp = "";
                             skip = true;
                         }
@@ -41,15 +41,15 @@ public class Lexer {
                     }
                     case '+': {
                         tokens.add(lexTemp(temp));
-                        tokens.add(new Token("ADDITION", "+"));
+                        tokens.add(new Token("ADDITION", "+",  lineNumber));
                         temp = "";
                         skip = true;
                         break;
                     }
                     case '-': {
-                        if(currentLine.charAt(i - 1) != 'L') {
+                        if (currentLine.charAt(i - 1) != 'L') {
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("SUBTRACTION", "-"));
+                            tokens.add(new Token("SUBTRACTION", "-",  lineNumber));
                             temp = "";
                             skip = true;
                         }
@@ -57,122 +57,122 @@ public class Lexer {
                     }
                     case '*': {
                         tokens.add(lexTemp(temp));
-                        tokens.add(new Token("MULTIPLICATION", "*"));
+                        tokens.add(new Token("MULTIPLICATION", "*",  lineNumber));
                         temp = "";
                         skip = true;
                         break;
                     }
                     case '/': {
                         tokens.add(lexTemp(temp));
-                        tokens.add(new Token("DIVISION", "/"));
+                        tokens.add(new Token("DIVISION", "/",  lineNumber));
                         temp = "";
                         skip = true;
                         break;
                     }
                     case '(': {
                         tokens.add(lexTemp(temp));
-                        tokens.add(new Token("L_PARENTHESES", "("));
+                        tokens.add(new Token("L_PARENTHESES", "(",  lineNumber));
                         temp = "";
                         skip = true;
                         break;
                     }
                     case ')': {
                         tokens.add(lexTemp(temp));
-                        tokens.add(new Token("R_PARENTHESES", ")"));
+                        tokens.add(new Token("R_PARENTHESES", ")",  lineNumber));
                         temp = "";
                         skip = true;
                         break;
                     }
                     case '{': {
                         tokens.add(lexTemp(temp));
-                        tokens.add(new Token("L_BRACES", "{"));
+                        tokens.add(new Token("L_BRACES", "{",  lineNumber));
                         temp = "";
                         skip = true;
                         break;
                     }
                     case '}': {
                         tokens.add(lexTemp(temp));
-                        tokens.add(new Token("R_BRACES", "}"));
+                        tokens.add(new Token("R_BRACES", "}",  lineNumber));
                         temp = "";
                         skip = true;
                         break;
                     }
                     case '&': {
-                        if(currentLine.charAt(i + 1) == '&'){
+                        if (currentLine.charAt(i + 1) == '&') {
                             i++;
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("AND", "&&"));
+                            tokens.add(new Token("AND", "&&",  lineNumber));
                             temp = "";
                             skip = true;
                         }
                         break;
                     }
                     case '>': {
-                        if(currentLine.charAt(i + 1) == '='){
+                        if (currentLine.charAt(i + 1) == '=') {
                             i++;
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("GREATER_EQUAL", ">="));
+                            tokens.add(new Token("GREATER_EQUAL", ">=",  lineNumber));
                             temp = "";
                             skip = true;
                         } else {
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("GREATER_THAN", ">"));
+                            tokens.add(new Token("GREATER_THAN", ">",  lineNumber));
                             temp = "";
                             skip = true;
                         }
                         break;
                     }
                     case '<': {
-                        if(currentLine.charAt(i + 1) == '='){
+                        if (currentLine.charAt(i + 1) == '=') {
                             i++;
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("LESS_EQUAL", "<="));
+                            tokens.add(new Token("LESS_EQUAL", "<=",  lineNumber));
                             temp = "";
                             skip = true;
                         } else {
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("LESS_THAN", "<"));
+                            tokens.add(new Token("LESS_THAN", "<",  lineNumber));
                             temp = "";
                             skip = true;
                         }
                         break;
                     }
                     case '!': {
-                        if(currentLine.charAt(i + 1) == '='){
+                        if (currentLine.charAt(i + 1) == '=') {
                             i++;
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("NOT_EQUAL", "!="));
+                            tokens.add(new Token("NOT_EQUAL", "!=",  lineNumber));
                             temp = "";
                             skip = true;
                         } else {
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("NOT", "!"));
+                            tokens.add(new Token("NOT", "!",  lineNumber));
                             temp = "";
                             skip = true;
                         }
                         break;
                     }
                     case '|': {
-                        if(currentLine.charAt(i + 1) == '|'){
+                        if (currentLine.charAt(i + 1) == '|') {
                             i++;
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("OR", "||"));
+                            tokens.add(new Token("OR", "||",  lineNumber));
                             temp = "";
                             skip = true;
                         } else {
                             tokens.add(lexTemp(temp));
-                            tokens.add(new Token("SPLIT", "|"));
+                            tokens.add(new Token("SPLIT", "|",  lineNumber));
                             temp = "";
                             skip = true;
                         }
                         break;
                     }
-                    case '"':{
+                    case '"': {
                         tokens.add(lexTemp(temp));
                         temp = "\"";
-                        for(int j = i + 1; j < currentLine.length(); j++){
-                            if(currentLine.charAt(j) == '"'){
-                                tokens.add(new Token("STRING", temp + "\""));
+                        for (int j = i + 1; j < currentLine.length(); j++) {
+                            if (currentLine.charAt(j) == '"') {
+                                tokens.add(new Token("STRING", temp + "\"",  lineNumber));
                                 i = j;
                             } else temp += currentLine.charAt(j);
                         }
@@ -181,17 +181,18 @@ public class Lexer {
                         break;
                     }
                 }
-                if(!skip)
+                if (!skip)
                     temp += currentLine.charAt(i);
             }
         }
+        lineNumber = -1;
         tokens = checkForEmptyTokens(tokens);
         return tokens;
     }
 
-    public static ArrayList<Token> checkForEmptyTokens(ArrayList<Token> tokens){
-        for(int i = 0; i < tokens.size(); i++){
-            if(tokens.get(i).key.equals("")){
+    public static ArrayList<Token> checkForEmptyTokens(ArrayList<Token> tokens) {
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens.get(i).key.equals("")) {
                 tokens.remove(i);
                 i--;
             }
@@ -199,48 +200,48 @@ public class Lexer {
         return tokens;
     }
 
-    public static Token lexTemp(String temp){
+    public static Token lexTemp(String temp) {
         Token token = new Token("", "");
-        if(temp.equals("print")){
-            return new Token("PRINT", "print");
-        } else if(temp.equals("int")){
-            return new Token("INT_T", "int");
-        } else if(temp.equals("decimal")){
-            return new Token("DECIMAL_T", "decimal");
-        }else if(temp.equals("string")){
-            return new Token("STRING_T", "string");
-        }else if(temp.equals("bool")){
-            return new Token("BOOL_T", "bool");
-        }else if(temp.equals("if")){
-            return new Token("IF", "if");
-        }else if(temp.equals("else")){
-            return new Token("ELSE", "else");
-        }else if(temp.equals("for")){
-            return new Token("FOR", "for");
-        }else if(temp.equals("goto")){
-            return new Token("GOTO", "goto");
-        }else if(temp.equals("getInt")){
-            return new Token("GET_INT", "getInt");
-        }else if(temp.equals("getDecimal")){
-            return new Token("GET_DECIMAL", "getDecimal");
-        }else if(temp.equals("getString")){
-            return new Token("GET_STRING", "getString");
-        }else if(temp.equals("getBool")){
-            return new Token("GET_BOOL", "getBool");
+        if (temp.equals("print")) {
+            return new Token("PRINT", "print",  lineNumber);
+        } else if (temp.equals("int")) {
+            return new Token("INT_T", "int",  lineNumber);
+        } else if (temp.equals("decimal")) {
+            return new Token("DECIMAL_T", "decimal",  lineNumber);
+        } else if (temp.equals("string")) {
+            return new Token("STRING_T", "string",  lineNumber);
+        } else if (temp.equals("bool")) {
+            return new Token("BOOL_T", "bool",  lineNumber);
+        } else if (temp.equals("if")) {
+            return new Token("IF", "if",  lineNumber);
+        } else if (temp.equals("else")) {
+            return new Token("ELSE", "else",  lineNumber);
+        } else if (temp.equals("for")) {
+            return new Token("FOR", "for",  lineNumber);
+        } else if (temp.equals("goto")) {
+            return new Token("GOTO", "goto",  lineNumber);
+        } else if (temp.equals("getInt")) {
+            return new Token("GET_INT", "getInt",  lineNumber);
+        } else if (temp.equals("getDecimal")) {
+            return new Token("GET_DECIMAL", "getDecimal",  lineNumber);
+        } else if (temp.equals("getString")) {
+            return new Token("GET_STRING", "getString",  lineNumber);
+        } else if (temp.equals("getBool")) {
+            return new Token("GET_BOOL", "getBool",  lineNumber);
         } else if (temp.matches("(\".*\")")) {
-            return new Token("STRING", temp);
+            return new Token("STRING", temp,  lineNumber);
         } else if (temp.equals("true") || temp.equals("false")) {
-            return new Token("BOOL", temp);
+            return new Token("BOOL", temp,  lineNumber);
         } else if (temp.matches("\\d+")) {
-            return new Token("INT", temp);
+            return new Token("INT", temp,  lineNumber);
         } else if (temp.matches("\\d+(\\.\\d{1,2})?")) {
-            return new Token("DECIMAL", temp);
+            return new Token("DECIMAL", temp,  lineNumber);
         } else if (temp.matches("^L-.*")) {
-            return new Token("L_GOTO", temp);
-        } else if (temp.matches(".*") &&temp != "") {
-            Token token1 = new Token("NAME", temp);
-            token1.posInLine = posInLine;
-            return new Token("NAME", temp);
+            return new Token("L_GOTO", temp,  lineNumber);
+        } else if (temp.matches(".*") && temp != "") {
+            Token token1 = new Token("NAME", temp,  lineNumber);
+            token1.posInLine = 1;
+            return token1;
         }
         return token;
     }
